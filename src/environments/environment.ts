381c3e138fe8c563e043f5b088104f654ffab692
Aniksaha-ssl <anik.saha@sslwireless.com>
2026-09-19 14:23:51 +0600

    Enviorment changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiBaseUrl: 'https://travelbooking.infinitycodehubltd.com/public/api',
   imageBaseUrl: 'https://travelbooking.infinitycodehubltd.com/public/storage/',
-  mcpServerUrl: 'http://127.0.0.1:8000/api',
+  mcpServerUrl: 'https://travel-mcp.infinitycodehubltd.com/public/api',
 };
